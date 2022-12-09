fd66c07af9ca52692e715efdca6f368c058f2781
Mitesh <mrvasoya2001@gmail.com>
2022-12-09 10:17:10 +0530

    Footer Fixes at Bottom...

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,13 +2,9 @@ import React from "react";
 
 
 export const Footer = () =>{
-    let footerStyle = {
-        position : "fix",
-        top : "100vh",
-        width : "100%"
-    }
+
     return (
-        <footer className="bg-dark text-light py-1" style={footerStyle}>
+        <footer className="bg-dark text-light py-3 footer" >
             <p className="text-center">
                 Copyright &copy; MyToDoList.org
             </p>
